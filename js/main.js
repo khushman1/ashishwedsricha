388dbee89598facd59fff44b11cbdda5fdf232ba
Khushman Patel <khushman1@gmail.com>
2018-03-19 12:56:56 +0530

    Set date for countdown timer

--- js/main.js
+++ js/main.js
@@ -252,7 +252,7 @@
 	
 
 	// Set the date we're counting down to
-		var countDownDate = new Date("Dec 02, 2017 15:37:25").getTime();
+		var countDownDate = new Date("Apr 18, 2018 19:00:30").getTime();
 
 		// Update the count down every 1 second
 		var x = setInterval(function() {
