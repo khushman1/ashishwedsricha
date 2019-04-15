c946fc080367490e048803a0c72c956b6cf88c0f
Khushman <khushman1@gmail.com>
2019-04-15 18:55:20 -0400

    Fixed countdown timer to show time elapsed and header text.

--- js/main.js
+++ js/main.js
@@ -252,7 +252,7 @@
 	
 
 	// Set the date we're counting down to
-		var countDownDate = new Date("Apr 18, 2018 19:00:30").getTime();
+		var countDownDate = new Date("Apr 18, 2018 19:00:00").getTime();
 
 		// Update the count down every 1 second
 		var x = setInterval(function() {
@@ -261,13 +261,13 @@
 		var now = new Date().getTime();
 
 		// Find the distance between now an the count down date
-		var distance = countDownDate - now;
+		var distance = now - countDownDate;
 
 		// Time calculations for days, hours, minutes and seconds
 		var days = Math.floor(distance / (1000 * 60 * 60 * 24));
 		var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 		var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-		var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        // var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
 		// Display the result in an element with id="demo"
 		// document.getElementById("demo").innerHTML = days + "Days " + hours + "Hours "
@@ -277,14 +277,13 @@
 		document.getElementById("days").innerHTML = days +" <small>days</small>";
 		document.getElementById("hours").innerHTML = hours + " <small>hours</small> ";
 		document.getElementById("minutes").innerHTML = minutes + " <small>minutes</small> ";
-		document.getElementById("seconds").innerHTML = seconds + " <small>seconds</small> ";
 
 		// If the count down is finished, write some text 
 		if (distance < 0) {
 		 clearInterval(x);
 		 document.getElementById("demo").innerHTML = "The Wedding Ceremony has begun!";
 		}
-		}, 1000);	
+		}, 60000);	
 	
 		
 	var bgVideo = function() {
